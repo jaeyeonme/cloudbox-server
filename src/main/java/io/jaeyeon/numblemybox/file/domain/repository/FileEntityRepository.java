b2381014a5544d7b21024bb64cebd7b523a003ad
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-10-23 01:07:02 +0900

    코드 리팩토링

--- src/main/java/io/jaeyeon/numblemybox/file/domain/repository/FileEntityRepository.java
+++ src/main/java/io/jaeyeon/numblemybox/file/domain/repository/FileEntityRepository.java
@@ -1,8 +1,10 @@
 package io.jaeyeon.numblemybox.file.domain.repository;
 
-import io.jaeyeon.numblemybox.file.domain.entity.FileEntity;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.jaeyeon.numblemybox.file.domain.entity.FileEntity;
 
 public interface FileEntityRepository extends JpaRepository<FileEntity, Long> {
 
--- src/main/java/io/jaeyeon/numblemybox/folder/domain/entity/Folder.java
+++ src/main/java/io/jaeyeon/numblemybox/folder/domain/entity/Folder.java
@@ -1,5 +1,8 @@
 package io.jaeyeon.numblemybox.folder.domain.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.jaeyeon.numblemybox.file.domain.entity.FileEntity;
 import io.jaeyeon.numblemybox.member.domain.entity.Member;
 import jakarta.persistence.CascadeType;
@@ -13,8 +16,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
