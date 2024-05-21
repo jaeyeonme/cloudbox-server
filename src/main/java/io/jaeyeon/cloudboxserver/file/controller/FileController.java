168faea2e990bd2d93628db6499991addc9ab0d6
Jaeyeon Cho <cjyeon1022@gmail.com>
2024-05-22 04:02:53 +0900

    [#8] Thymeleaf 를 사용하여 View 작성

--- src/main/java/io/jaeyeon/cloudboxserver/file/controller/FileController.java
+++ src/main/java/io/jaeyeon/cloudboxserver/file/controller/FileController.java
@@ -1,9 +1,46 @@
 package io.jaeyeon.cloudboxserver.file.controller;
 
+import io.jaeyeon.cloudboxserver.file.domain.entity.FileEntity;
+import io.jaeyeon.cloudboxserver.file.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
-@RequestMapping("/api/files")
+@RequiredArgsConstructor
+@RequestMapping("/files")
 public class FileController {
+
+    @Value("${file.storage-directory}")
+    private String uploadDir;
+
+    private final FileService fileService;
+
+    @GetMapping("/")
+    public String listFiles(Model model) {
+        List<FileEntity> files = fileService.listFiles();
+        model.addAttribute("files", files);
+        return "fileList";
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
+        fileService.upload(file);
+        redirectAttributes.addFlashAttribute("message", "File uploaded successfully: " + file.getOriginalFilename());
+        return "redirect:/files/";
+    }
+
+    @GetMapping("/download")
+    @ResponseBody
+    public Resource downloadFile(@RequestParam String fileName) throws IOException {
+        return fileService.downloadFile(fileName);
+    }
 }
