ef9560dc531efc8dc2ff4e5e8c5a077bf85f69a5
Jaeyeon Cho <cjyeon1022@gmail.com>
2024-05-22 18:00:00 +0900

    [#8] Util 클래스 리팩토링

--- src/main/java/io/jaeyeon/cloudboxserver/common/FileUtility.java
+++ src/main/java/io/jaeyeon/cloudboxserver/common/FileUtility.java
@@ -4,28 +4,26 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import org.springframework.stereotype.Component;
 
-@Component
 public class FileUtility {
 
-  public boolean exists(Path path) {
+  public static boolean exists(Path path) {
     return Files.exists(path);
   }
 
-  public String probeContentType(Path path) throws IOException {
+  public static String probeContentType(Path path) throws IOException {
     return Files.probeContentType(path);
   }
 
-  public void createDirectories(Path path) throws IOException {
+  public static void createDirectories(Path path) throws IOException {
     Files.createDirectories(path);
   }
 
-  public String createFilePath(String fileName) {
+  public static String createFilePath(String fileName) {
     return Paths.get(fileName).toString();
   }
 
-  public void deleteFile(Path path) throws IOException {
+  public static void deleteFile(Path path) throws IOException {
     Files.deleteIfExists(path);
   }
 }
